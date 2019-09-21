84290a7f1ea7ee0850c33021c4221c255aed1c63
Lars Karbo <mail@larskarbo.no>
2019-09-21 16:58:40 +0200

    Updates

---
+++ precache-manifest.0a303c5c20548b24c63cfeef73028449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b929bf7620dce25e04c471d66cbc088",
+    "url": "/focu/index.html"
+  },
+  {
+    "revision": "32c32c65baebc97a56db",
+    "url": "/focu/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8423e0a2ad28d2bc21a4",
+    "url": "/focu/static/js/2.964eee3f.chunk.js"
+  },
+  {
+    "revision": "32c32c65baebc97a56db",
+    "url": "/focu/static/js/main.3963d767.chunk.js"
+  },
+  {
+    "revision": "36a9343bb8a13b3278db",
+    "url": "/focu/static/js/runtime-main.732e65a0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/focu/static/media/logo.25bf045c.svg"
+  }
+]);
